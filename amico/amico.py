158087680f452434033f710f86a1ec43289e2d0e
David Czarnecki <me@davidczarnecki.com>
2013-01-03 09:42:50 -0500

    Added follow and unfollow methods and tests

--- amico/amico.py
+++ amico/amico.py
@@ -4,8 +4,93 @@ class Amico(object):
   VERSION = '1.0.0'
 
   DEFAULTS = {
+    'namespace': 'amico',
+    'following_key': 'following',
+    'followers_key': 'followers',
+    'blocked_key': 'blocked',
+    'blocked_by_key': 'blocked_by',
+    'reciprocated_key': 'reciprocated',
+    'pending_key': 'pending',
+    'pending_with_key': 'pending_with',
+    'pending_follow': False,
+    'default_scope_key': 'default',
+    'page_size': 25
   }
 
-  def __init__(self, default_options = DEFAULTS):
+  def __init__(self, default_options = DEFAULTS, redis_connection = None):
     self.options = Amico.DEFAULTS.copy()
     self.options.update(default_options)
+    if redis_connection == None:
+      self.redis_connection = redis.StrictRedis(host = 'localhost', port = 6379, db = 0)
+    else:
+      self.redis_connection = redis_connection
+
+  def follow(self, from_id, to_id, scope = None):
+    if scope == None:
+      scope = self.options['default_scope_key']
+
+    if from_id == to_id:
+      return
+    if self.is_blocked(to_id, from_id, scope):
+      return
+    if self.options['pending_follow'] and self.is_pending(from_id, to_id, scope):
+      return
+
+    if self.options['pending_follow']:
+      transaction = self.redis_connection.pipeline()
+      transaction.zadd('%s:%s:%s:%s' % (self.options['namespace'], self.options['pending_key'], scope, to_id), 33, from_id)
+      transaction.zadd('%s:%s:%s:%s' % (self.options['namespace'], self.options['pending_with_key'], scope, from_id), 33, to_id)
+      transaction.execute()
+    else:
+      self.__add_following_followers_reciprocated(from_id, to_id, scope)
+
+  def unfollow(self, from_id, to_id, scope = None):
+    if scope == None:
+      scope = self.options['default_scope_key']
+
+    if from_id == to_id:
+      return
+
+    transaction = self.redis_connection.pipeline()
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['following_key'], scope, from_id), to_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['followers_key'], scope, to_id), from_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['reciprocated_key'], scope, from_id), to_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['reciprocated_key'], scope, to_id), from_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['pending_key'], scope, to_id), from_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['pending_with_key'], scope, from_id), to_id)
+    transaction.execute()
+
+  def is_blocked(self, id, blocked_id, scope = None):
+    if scope == None:
+      scope = self.options['default_scope_key']
+
+    return self.redis_connection.zscore('%s:%s:%s:%s' % (self.options['namespace'], self.options['blocked_key'], scope, id), blocked_id) != None
+
+  def is_following(self, id, following_id, scope = None):
+    if scope == None:
+      scope = self.options['default_scope_key']
+
+    return self.redis_connection.zscore('%s:%s:%s:%s' % (self.options['namespace'], self.options['following_key'], scope, id), following_id) != None
+
+  def is_reciprocated(self, from_id, to_id, scope = None):
+    if scope == None:
+      scope = self.options['default_scope_key']
+
+    return self.is_following(from_id, to_id, scope) and self.is_following(to_id, from_id, scope)
+
+  def __add_following_followers_reciprocated(self, from_id, to_id, scope = None):
+    if scope == None:
+      scope = self.options['default_scope_key']
+
+    transaction = self.redis_connection.pipeline()
+    transaction.zadd('%s:%s:%s:%s' % (self.options['namespace'], self.options['following_key'], scope, from_id), 33, to_id)
+    transaction.zadd('%s:%s:%s:%s' % (self.options['namespace'], self.options['followers_key'], scope, to_id), 33, from_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['pending_key'], scope, to_id), 33, from_id)
+    transaction.zrem('%s:%s:%s:%s' % (self.options['namespace'], self.options['pending_with_key'], scope, from_id), 33, to_id)
+    transaction.execute()
+
+    if self.is_reciprocated(from_id, to_id):
+      transaction = self.redis_connection.pipeline()
+      transaction.zadd('%s:%s:%s:%s' % (self.options['namespace'], self.options['reciprocated_key'], scope, from_id), 33, to_id)
+      transaction.zadd('%s:%s:%s:%s' % (self.options['namespace'], self.options['reciprocated_key'], scope, to_id), 33, from_id)
+      transaction.execute()
--- test/amico/amico_test.py
+++ test/amico/amico_test.py
@@ -2,15 +2,66 @@ import unittest
 import time
 import sure
 
-from redis import Redis
+import redis
 from amico import Amico
 
 class AmicoTest(unittest.TestCase):
   def setUp(self):
-    pass
+    self.redis_connection = redis.StrictRedis(host = 'localhost', port = 6379, db = 15)
 
   def tearDown(self):
-    pass
+    self.redis_connection.flushdb()
 
   def test_version(self):
     Amico.VERSION.should.equal('1.0.0')
+
+  def test_amico_defaults(self):
+    Amico.DEFAULTS['namespace'].should.equal('amico')
+    Amico.DEFAULTS['following_key'].should.equal('following')
+    Amico.DEFAULTS['followers_key'].should.equal('followers')
+    Amico.DEFAULTS['blocked_key'].should.equal('blocked')
+    Amico.DEFAULTS['blocked_by_key'].should.equal('blocked_by')
+    Amico.DEFAULTS['reciprocated_key'].should.equal('reciprocated')
+    Amico.DEFAULTS['pending_key'].should.equal('pending')
+    Amico.DEFAULTS['pending_with_key'].should.equal('pending_with')
+    Amico.DEFAULTS['pending_follow'].should.be.false
+    Amico.DEFAULTS['default_scope_key'].should.equal('default')
+    Amico.DEFAULTS['page_size'].should.equal(25)
+
+  # follow tests
+  def test_it_should_allow_you_to_follow(self):
+    amico = Amico(redis_connection = self.redis_connection)
+    amico.follow(1, 11)
+
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['following_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(1)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['followers_key'], Amico.DEFAULTS['default_scope_key'], 11)).should.equal(1)
+
+  def test_it_should_not_allow_you_to_follow_yourself(self):
+    amico = Amico(redis_connection = self.redis_connection)
+    amico.follow(1, 1)
+
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['following_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(0)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['followers_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(0)
+
+  def test_it_should_add_each_individual_to_the_reciprocated_set_if_you_both_follow_each_other(self):
+    amico = Amico(redis_connection = self.redis_connection)
+    amico.follow(1, 11)
+    amico.follow(11, 1)
+
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['reciprocated_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(1)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['reciprocated_key'], Amico.DEFAULTS['default_scope_key'], 11)).should.equal(1)
+
+  # unfollow tests
+  def test_it_should_allow_you_to_unfollow(self):
+    amico = Amico(redis_connection = self.redis_connection)
+    amico.follow(1, 11)
+
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['following_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(1)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['followers_key'], Amico.DEFAULTS['default_scope_key'], 11)).should.equal(1)
+
+    amico.unfollow(1, 11)
+
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['following_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(0)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['followers_key'], Amico.DEFAULTS['default_scope_key'], 11)).should.equal(0)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['reciprocated_key'], Amico.DEFAULTS['default_scope_key'], 1)).should.equal(0)
+    amico.redis_connection.zcard('%s:%s:%s:%s' % (Amico.DEFAULTS['namespace'], Amico.DEFAULTS['reciprocated_key'], Amico.DEFAULTS['default_scope_key'], 11)).should.equal(0)
